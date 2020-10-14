ad7fd61977f7a307da240f48f051ff7a8dc4cf7c
Xavier Laviron <xavier.laviron@gmx.fr>
2020-10-14 11:18:55 +0200

    move files

---
+++ advanced_js/part1_introduction/chap2_structure_donnees/01_reference_array.js
@@ -0,0 +1,36 @@
+let fruits =  ['Apple', 'Orange'];
+
+let newFruits = fruits;
+
+newFruits.push('Banana')
+
+console.log(newFruits.length === fruits.length)
+
+newFruits = [ ...fruits ];
+
+fruits.push('Kiwi')
+
+console.log(newFruits.length === fruits.length)
+
+
+console.log('Question 2 ********************************************************');
+const populations = [
+    { "id": 0, "name": "Alan" },
+    { "id": 1, "name": "Albert" },
+    { "id": 2, "name": "Jhon" },
+    { "id": 3, "name": "Brice" },
+    { "id": 4, "name": "Alexendra" },
+    { "id": 5, "name": "Brad" },
+    { "id": 6, "name": "Carl" },
+    { "id": 7, "name": "Dallas" },
+    { "id": 8, "name": "Dennis" },
+    { "id": 9, "name": "Edgar" },
+    { "id": 10, "name": "Erika" },
+    { "id": 11, "name": "Isaac" },
+    { "id": 12, "name": "Ian" }
+];
+
+const populations2 = populations;
+populations[0].lastname = 'Poe';
+
+console.log(populations2);
